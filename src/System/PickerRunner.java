7a93b6fd8ad02f0e3eda0f458af779c6250dc160
Sean Passmore <jeanghandi@gmail.com>
2019-07-18 17:00:12 -0400

    Add populate MovieDirectors,MovieTags,Tags

--- src/System/PickerRunner.java
+++ src/System/PickerRunner.java
@@ -3,51 +3,122 @@ import java.io.*;
 import java.sql.*;
 import java.util.TimeZone;
 
-public class PickerRunner
-{
-    public static void main(String args[]) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException
+public class PickerRunner {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
+        populateMovies();
+        populateMovieDirectors();
+        populateMovieTags();
+        populateTags();
+    }
+    public static void populateMovieTags() throws ClassNotFoundException, SQLException, FileNotFoundException, IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader("Rotten Tomatos Dataset/movie_tags.dat"));
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Movie_Matcher?serverTimezone="
+                + TimeZone.getDefault().getID(),"root", "assassass1");
+        Statement makeTable = con.createStatement();
+        String createMovieTagsTable = "CREATE TABLE MOVIE_TAG" +
+                "(movieId int,"+
+                "tagId int,"+
+                "tagWeight int,"+
+                "foreign key  (movieId) references Movie(id))";
+        makeTable.executeUpdate(createMovieTagsTable);
+        makeTable.close();
+        PreparedStatement insertMovieTags = con.prepareStatement("INSERT INTO MOVIE_TAG (movieId, tagId, tagWeight)"+
+                "VALUES (?, ?, ?)");
+        br.readLine();
+        String line;
+        int tagId;
+        int tagWeight;
+        int movieId;
+        while ((line = br.readLine()) != null)
+        {
+            String[] tokens = line.split("\t");
+            movieId = Integer.parseInt(tokens[0]);
+            tagId = Integer.parseInt(tokens[1]);
+            tagWeight = Integer.parseInt(tokens[2]);
+            insertMovieTags.setInt(1,movieId);
+            insertMovieTags.setInt(2,tagId);
+            insertMovieTags.setInt(3,tagWeight);
+            insertMovieTags.executeUpdate();
+        }
+        insertMovieTags.close();
+    }
+    public static void populateMovieDirectors() throws ClassNotFoundException, SQLException, FileNotFoundException, IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader("Rotten Tomatos Dataset/movie_directors.dat"));
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Movie_Matcher?serverTimezone="
+                + TimeZone.getDefault().getID(),"root", "assassass1");
+        Statement makeTable = con.createStatement();
+        String createMovieDirectorsTable = "CREATE TABLE MOVIE_DIRECTOR" +
+                "(movieId int not NULL,"+
+                "directorId varchar(50)not NULL,"+
+                "directorName varchar(50),"+
+                "primary key (movieId)," +
+                "foreign key  (movieId) references Movie(id))";
+        makeTable.executeUpdate(createMovieDirectorsTable);
+        makeTable.close();
+        PreparedStatement insertMovieDirector = con.prepareStatement("INSERT INTO MOVIE_DIRECTOR (movieId, directorId, DirectorName)"+
+                "VALUES (?, ?, ?)");
+        br.readLine();
+        String line;
+        String directorName;
+        String directorId;
+        int movieId;
+        while ((line = br.readLine()) != null)
+        {
+            String[] tokens = line.split("\t");
+            movieId = Integer.parseInt(tokens[0]);
+            directorId = tokens[1];
+            directorName = tokens[2];
+            insertMovieDirector.setInt(1,movieId);
+            insertMovieDirector.setString(2,directorId);
+            insertMovieDirector.setString(3,directorName);
+            insertMovieDirector.executeUpdate();
+        }
+        insertMovieDirector.close();
+    }
+    public static void populateMovies() throws ClassNotFoundException, SQLException, FileNotFoundException, IOException
     {
         BufferedReader br = new BufferedReader(new FileReader("Rotten Tomatos Dataset/movies.dat"));
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Movies?serverTimezone=" + TimeZone.getDefault().getID(),"root", "assassass1");
-        Statement dropAll = con.createStatement();
-        String drop = "DROP TABLE MOVIE";
-        dropAll.executeUpdate(drop);
-        dropAll.close();
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Movie_Matcher?serverTimezone="
+                + TimeZone.getDefault().getID(),"root", "assassass1");
         Statement makeTable = con.createStatement();
         String createMovieTable = "CREATE TABLE MOVIE"+
-                                    "(id integer not NULL,"+
-                                    "title varchar(300),"+
-                                    "imdbId integer,"+
-                                    "spanishTitle varchar(300),"+
-                                    "imdbPictureURL varchar(200),"+
-                                    "year integer,"+
-                                    "rtID varchar(300),"+
-                                    "rtAllCriticsRating decimal,"+
-                                    "rtAllCriticsNumReviews integer,"+
-                                    "rtAllCriticsNumFresh integer,"+
-                                    "rtAllCriticsNumRotten integer,"+
-                                    "rtAllCriticsScore decimal,"+
-                                    "rtTopCriticsRating decimal,"+
-                                    "rtTopCriticsNumReviews integer,"+
-                                    "rtTopCriticsNumFresh integer,"+
-                                    "rtTopCriticsNumRotten integer,"+
-                                    "rtTopCriticsScore decimal,"+
-                                    "rtAudienceRating decimal,"+
-                                    "rtAudienceNumRatings integer,"+
-                                    "rtAudienceScore integer,"+
-                                    "rtPictureURL varchar(200),"+
-                                    "primary key(id))";
+                "(id integer not NULL,"+
+                "title varchar(300),"+
+                "imdbId integer,"+
+                "spanishTitle varchar(300),"+
+                "imdbPictureURL varchar(200),"+
+                "year integer,"+
+                "rtID varchar(300),"+
+                "rtAllCriticsRating decimal,"+
+                "rtAllCriticsNumReviews integer,"+
+                "rtAllCriticsNumFresh integer,"+
+                "rtAllCriticsNumRotten integer,"+
+                "rtAllCriticsScore decimal,"+
+                "rtTopCriticsRating decimal,"+
+                "rtTopCriticsNumReviews integer,"+
+                "rtTopCriticsNumFresh integer,"+
+                "rtTopCriticsNumRotten integer,"+
+                "rtTopCriticsScore decimal,"+
+                "rtAudienceRating decimal,"+
+                "rtAudienceNumRatings integer,"+
+                "rtAudienceScore integer,"+
+                "rtPictureURL varchar(200),"+
+                "primary key(id))";
         makeTable.executeUpdate(createMovieTable);
         makeTable.close();
         PreparedStatement insertMovie = con.prepareStatement("INSERT INTO MOVIE (id,title,imdbId,spanishTitle,imdbPictureURL," +
-                                                                  "year, rtID, rtAllCriticsRating,rtAllCriticsNumReviews," +
-                                                                    "	rtAllCriticsNumFresh,	rtAllCriticsNumRotten,	" +
-                                                                    "rtAllCriticsScore,	rtTopCriticsRating,	rtTopCriticsNumReviews," +
-                                                                    "	rtTopCriticsNumFresh,	rtTopCriticsNumRotten,	" +
-                                                                    "rtTopCriticsScore,	rtAudienceRating,	rtAudienceNumRatings," +
-                                                                    "	rtAudienceScore,	rtPictureURL) " +
-                                                                  "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                "year, rtID, rtAllCriticsRating,rtAllCriticsNumReviews," +
+                "	rtAllCriticsNumFresh,	rtAllCriticsNumRotten,	" +
+                "rtAllCriticsScore,	rtTopCriticsRating,	rtTopCriticsNumReviews," +
+                "	rtTopCriticsNumFresh,	rtTopCriticsNumRotten,	" +
+                "rtTopCriticsScore,	rtAudienceRating,	rtAudienceNumRatings," +
+                "	rtAudienceScore,	rtPictureURL) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
         String line;
         String rtPictureURL;
@@ -194,6 +265,5 @@ public class PickerRunner
 
 
         }
-
     }
 }
